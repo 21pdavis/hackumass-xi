4233d1ff084ecdbd75e39ac91b9bab5692207aef
Benzillaist <benkuchma@gmail.com>
2023-11-11 17:52:27 -0500

    Commit involving player movement/teleportation + box and noise
    - Added controllable player that can teleport between two seemingly identical boxes.
    - Noise has been added whose opacity is controllable.
    
    Todo:
    - Make noise appear when teleporting
    - Make noise appear when close to hunter
    - Fix problem with meshcollider of noise moving the player around

---
+++ Assets/Flashbang.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashbang : MonoBehaviour
+{
+    [SerializeField] private GameObject myObj;
+    private float a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // myObj.GetComponent<Material>().shader.alpha = 30;
+        // Renderer rend = myObj.getComponent<Renderer>();
+        // Material mat = rend.getComponent<Material>();
+        // mat.a = 0;
+        // rend.material = mat;
+        // myObj.renderer.material = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            a = 10
+            Flash();
+        }
+        // GetComponent<Renderer>().material.color.a=Time.time-Mathf.Floor(Time.time);
+    }
+
+    // void FixedUpdate() {
+    //     float a = 
+    //     if(a == 1) {
+
+    //     }
+    // }
+
+    void Flash() {
+        for(int i = 0; i < 10; i++) {
+
+        }
+    }
+}
