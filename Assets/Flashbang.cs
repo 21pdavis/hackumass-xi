e82f5dfd7c60e2a98ff86c343371c43ef60d9148
Benzillaist <benkuchma@gmail.com>
2023-11-12 00:52:29 -0500

    added colors

--- Assets/Flashbang.cs
+++ Assets/Flashbang.cs
@@ -1,42 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// import Math;
 
 public class Flashbang : MonoBehaviour
 {
     [SerializeField] private GameObject myObj;
+
+    public Rigidbody2D P1;
+    public Rigidbody2D P2;
     private float a;
+    private float b;
+    private float d;
+    private float ra;
+    private float rb;
+    private float ba;
+    private float bb;
+    // private float ravg;
+    // private float bavg;
+
     // Start is called before the first frame update
     void Start()
     {
-        // myObj.GetComponent<Material>().shader.alpha = 30;
-        // Renderer rend = myObj.getComponent<Renderer>();
-        // Material mat = rend.getComponent<Material>();
-        // mat.a = 0;
-        // rend.material = mat;
-        // myObj.renderer.material = color;
+
+        Debug.Log(myObj.GetComponent<Renderer>().material.GetFloat("_alpha"));
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        d = System.Math.Min(System.Math.Min((P1.position - P2.position).magnitude, (P1.position - P2.position + new Vector2(0, -40)).magnitude), System.Math.Min((P1.position - P2.position + new Vector2(0, 40)).magnitude, 200));
+        b = 0.75f * (float)System.Math.Exp(-0.5f * (float)d);
+        if(P1.position[1] - P2.position[1] > 20) {
+            rb = 1.0f;
+        } else if(P1.position[1] - P2.position[1] < -20) {
+            bb = 1.0f;
+        } else {
+            rb = 0.0f;
+            ra = 0.0f;
+        }
         if(Input.GetKeyDown(KeyCode.Space)) {
-            a = 10
-            Flash();
+            if(P1.position[1] > 0) {
+                ra = 1.0f;
+            } else {
+                ba = 1.0f;
+            }
+            a = 1.0f;
         }
-        // GetComponent<Renderer>().material.color.a=Time.time-Mathf.Floor(Time.time);
-    }
-
-    // void FixedUpdate() {
-    //     float a = 
-    //     if(a == 1) {
-
-    //     }
-    // }
-
-    void Flash() {
-        for(int i = 0; i < 10; i++) {
-
+        if(a > 0) {
+            a -= 0.1f;
         }
+        if(ra > 0) {
+            ra -= 0.1f;
+        }
+        if(rb > 0) {
+            rb -= 0.1f;
+        }
+        if(ba > 0) {
+            ba -= 0.1f;
+        }
+        if(bb > 0) {
+            bb -= 0.1f;
+        }
+        myObj.GetComponent<Renderer>().material.SetFloat("_alpha", System.Math.Max(a, b));
+        myObj.GetComponent<Renderer>().material.SetFloat("_Red", (ra + rb) / 2);
+        myObj.GetComponent<Renderer>().material.SetFloat("_Blue", (ba + bb) / 2);
     }
 }
--- Assets/Rbmovement2d.cs
+++ Assets/Rbmovement2d.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rbmovement : MonoBehaviour {
+public class Rbmovement2d : MonoBehaviour
+{
     public float speed = 10.0f;
-    public Rigidbody rb;
+    public Rigidbody2D rb;
     public Vector2 movement;
 
     // Start is called before the first frame update
     void Start() {
-        rb = this.GetComponent<Rigidbody>();
+        rb = (Rigidbody2D)this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
